b62d63b5a3824020056a5247d87ba321c29ba167
GhausAliKhan <ghauskhan1616@gmail.com>
2024-01-18 20:39:52 +0500

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Greeting.create(message: 'Hi there!')
+Greeting.create(message: 'Hello, how are you?')
+Greeting.create(message: 'Hello World!')
+Greeting.create(message: 'Hello Human!')
+Greeting.create(message: 'Hola!')
